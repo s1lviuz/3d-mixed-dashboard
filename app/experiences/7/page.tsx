3e58de44cb371f33f051b4da8af2029cc40b4060
Silvio Jr <silviojraraujo@gmail.com>
2023-10-25 23:19:05 -0300

    Finished 7

--- app/experiences/7/page.tsx
+++ app/experiences/7/page.tsx
@@ -1,17 +1,32 @@
 'use client'
 
-import { Canvas, useFrame } from '@react-three/fiber'
-import { OrbitControls, useGLTF } from '@react-three/drei'
+import { Canvas, useFrame, extend } from '@react-three/fiber'
+import { shaderMaterial ,Center, OrbitControls, Sparkles, useGLTF, useTexture } from '@react-three/drei'
 import { useRef } from 'react'
-import { Mesh } from 'three'
+import { Color, Mesh } from 'three'
+import portalVertexShader from '@/shaders/portal/vertex.glsl'
+import portalFragmentShader from '@/shaders/portal/fragment.glsl'
+
+const PortalMaterial = shaderMaterial({
+    uTime: 0,
+    uColorStart: new Color('#ffffff'),
+    uColorEnd: new Color('#000000'),
+},portalVertexShader, portalFragmentShader)
+
+extend({ PortalMaterial })
 
 export function Experience() {
     const cube = useRef<Mesh>(null)
+    const portalMaterial = useRef<any>(null)
 
     const model = useGLTF('/model/portal.glb')
 
+    const bakedTexture = useTexture('/model/baked.jpg')
+
     useFrame((state, delta) => {
-        
+        if (portalMaterial.current) {
+            portalMaterial.current.uTime += delta
+        }
     })
 
     return <>
@@ -19,13 +34,48 @@ export function Experience() {
 
         <OrbitControls makeDefault />
 
-        <mesh geometry={model.nodes.baked.geometry} />
+        <Center>
+            <mesh geometry={model.nodes.baked.geometry}>
+                <meshBasicMaterial map={bakedTexture} map-flipY={false} />
+            </mesh>
+
+            <mesh geometry={model.nodes.poleLightA.geometry} position={model.nodes.poleLightA.position}>
+                <meshBasicMaterial color='ffffe5' />
+            </mesh>
+
+            <mesh geometry={model.nodes.poleLightB.geometry} position={model.nodes.poleLightB.position}>
+                <meshBasicMaterial color='ffffe5' />
+            </mesh>
+
+            <mesh geometry={model.nodes.portalLight.geometry} position={model.nodes.portalLight.position} rotation={model.nodes.portalLight.rotation}>
+                {/* <shaderMaterial 
+                    vertexShader={portalVertexShader}
+                    fragmentShader={portalFragmentShader}
+                    uniforms={{
+                        uTime: { value: 0 },
+                        uColorStart: { value: new Color('#ffffff') },
+                        uColorEnd: { value: new Color('#000000') }
+                    }}
+                /> */}
+
+                <portalMaterial ref={portalMaterial} />
+            </mesh>
+
+            <Sparkles
+                size={6}
+                scale={[4,2,4]}
+                position-y={1}
+                speed={0.2}
+                count={40}
+            />
+        </Center>
     </>
 }
 
 export default function App() {
     return (
         <Canvas
+            flat
             camera={{
                 fov: 45,
                 near: 0.1,
--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     transpilePackages: ['three'],
+    webpack: (config, options) => {
+        config.module.rules.push({
+            test: /\.glsl/,
+            type: "asset/source",
+        })
+        return config
+    },
 }
 
 module.exports = nextConfig
